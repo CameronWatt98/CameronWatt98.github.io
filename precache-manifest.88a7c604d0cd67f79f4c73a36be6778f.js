889e4b2e76522800a1783a7ca776cffc88d928cd
Cameron Watt <s1637380@ed.ac.uk>
2020-01-12 16:37:11 +0000

    Website Attempt 1

---
+++ precache-manifest.88a7c604d0cd67f79f4c73a36be6778f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5af601919ff4f1f6741d1cad2cac0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ca085d5a12c291ceee",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6d9cb9e4aaf36e8a234b",
+    "url": "/static/css/main.f0ea082d.chunk.css"
+  },
+  {
+    "revision": "95ca085d5a12c291ceee",
+    "url": "/static/js/2.276d27b5.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/static/js/2.276d27b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d9cb9e4aaf36e8a234b",
+    "url": "/static/js/main.4e3bdb7d.chunk.js"
+  },
+  {
+    "revision": "fa109d2ecb354f86a5c3",
+    "url": "/static/js/runtime-main.c911de06.js"
+  },
+  {
+    "revision": "0afcb0f3c0a6024bf536045147182f16",
+    "url": "/static/media/AP1_4531.0afcb0f3.jpg"
+  },
+  {
+    "revision": "f21cd7c8e4dbafdb36f3a15f2858c1d5",
+    "url": "/static/media/AP1_4595.f21cd7c8.jpg"
+  },
+  {
+    "revision": "4167555c226d7c30bdb476275870e3b2",
+    "url": "/static/media/AP1_4613.4167555c.jpg"
+  },
+  {
+    "revision": "7ae17f160bf7159eb62966851510c862",
+    "url": "/static/media/Messenger_Logo_White_RGB.7ae17f16.png"
+  },
+  {
+    "revision": "aacbec2372c8bbcdd2b992439af9bbbb",
+    "url": "/static/media/Spotify_Logo_RGB_Green.aacbec23.png"
+  },
+  {
+    "revision": "08685ee8d03c9225118ce439d463a32c",
+    "url": "/static/media/TransparentLogo.08685ee8.png"
+  },
+  {
+    "revision": "5d6ea38a769498dfc19fe6389d14db39",
+    "url": "/static/media/f_logo_RGB-Blue_1024.5d6ea38a.png"
+  },
+  {
+    "revision": "fc9685182715848864fea96a6a995e6f",
+    "url": "/static/media/iglogo.fc968518.png"
+  },
+  {
+    "revision": "746241b264e6ffd92e6c581118762fc9",
+    "url": "/static/media/ndlogo.746241b2.png"
+  }
+]);
